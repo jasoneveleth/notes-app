44a6624e3df422123a6502c6156475844f1675d6
Jason Eveleth <jj11@eveleth.com>
2023-01-18 23:10:10 -0500

    Fix notes dir and the /list filenames

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,20 +1,21 @@
 const express = require('express')
 const fs      = require('fs')
 const router  = express.Router()
+const os = require("os");
 
-const NOTESDIRECTORY = {root:'.'}
+const NOTESDIRECTORY = os.homedir() + '/notes'
 
 router.use(express.json())
 
 router.get('/list', (req, res) => {
-    const filenames = fs.readdirSync(".", NOTESDIRECTORY)
-    res.send({names:filenames})
+    const filenames = fs.readdirSync(NOTESDIRECTORY)
+    res.send({names: filenames.filter((f) => f.split('.').pop() == "md")})
 })
 
 router.get('/contents', (req, res) => {
     const filename = req.query.filename 
     try{
-        res.sendFile(filename, NOTESDIRECTORY)
+        res.sendFile(filename, {root: NOTESDIRECTORY})
     }catch{
         res.send({error:`file failed to open ${filename}`})
     }
@@ -24,7 +25,7 @@ router.get('/append', (req, res) => {
     const filename = req.query.filename // full path
     const content  = req.query.contents
     try{
-       fs.appendFileSync(`./${filename}`, content)
+       fs.appendFileSync(`./${filename}`, content, {root: NOTESDIRECTORY})
        res.send({"status":"ok"})
     }catch(err){
         console.log(err)
