8b2fb09b6149bbf68a7b2ce0c284f929702e37d0
Jason Eveleth <jj11@eveleth.com>
2023-01-20 10:42:10 -0500

    cors

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,14 @@ const express = require('express')
 const app = express()
 const os = require('os')
 const path = require('path')
-const cors = Require('cors')
+const cors = require('cors')
 
 const PORT = process.env.PORT || 25565;
 const api = require("./api")
 const frontend_dir = "../view/dist"
 const images_dir = path.join(os.homedir(), "notes", "images")
 
-app.use('cors') 
+app.use(cors) 
 
 app.use('/api', api)
 app.use(express.static(frontend_dir))
