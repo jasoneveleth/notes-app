a9bdde6155f92c53c62175ba0cf07f21ecd7f329
Jason Eveleth <jj11@eveleth.com>
2023-01-20 10:55:52 -0500

    fixed global

--- view/src/main.js
+++ view/src/main.js
@@ -7,5 +7,6 @@ const app = createApp(App)
 
 // default options config: { expires: '1d', path: '/', domain: '', secure: '', sameSite: 'Lax' }
 app.use(VueCookies, { expires: '7d'})
+app.config.globalProperties.$host = process.env.API || "http://aqua.jason.cash:25565"
 
 app.mount('#app')
